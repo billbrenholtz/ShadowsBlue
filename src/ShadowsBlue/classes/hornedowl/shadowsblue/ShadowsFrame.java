e07c576d554025b6ee066507c4c4387e695cdfb7
billb <billbrenholtz@gmail.com>
2019-02-16 16:48:20 -0700

    fix frame structure

--- src/ShadowsBlue/classes/hornedowl/shadowsblue/ShadowsFrame.java
+++ src/ShadowsBlue/classes/hornedowl/shadowsblue/ShadowsFrame.java
@@ -11,22 +11,16 @@ import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 /**
  *
  * @author liberty
  */
-class ShadowsFrame extends JFrame implements MouseListener, KeyListener {
+class ShadowsFrame extends JFrame {
 
     private final PropertyDude myDude;
     private final TransformAnim myAnim;
@@ -39,7 +33,7 @@ class ShadowsFrame extends JFrame implements MouseListener, KeyListener {
         GraphicsConfiguration gc = gd.getDefaultConfiguration();
         screenRect = gc.getBounds();
 
-        myAnim = new TransformAnim();
+        myAnim = new TransformAnim(this, screenRect);
         myAnim.setPreferredSize(new Dimension(screenRect.width, myAnim.getBih()));
         myDude = new PropertyDude();
 
@@ -71,56 +65,8 @@ class ShadowsFrame extends JFrame implements MouseListener, KeyListener {
                 myAnim.stop();
             }
         });
-        addMouseListener(this);
-        addKeyListener(this);
-
         pack();
 
-    }
-
-    //display full image when image is clicked on
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-        if (myAnim.getMovers().isEmpty()) {
-            return;
-        }
-
-        int xpos = e.getX();
-        int ypos = e.getY();
-        Thimg xthimg = null;
-        for (int i = 0; i < myAnim.getMovers().size(); i++) {
-            //Determine who was clicked on, if any, by x position
-            if (xpos >= myAnim.getMovers().get(i).getX() && xpos < (myAnim.getMovers().get(i).getX() + myAnim.getMovers().get(i).getThumbWidth())) {
-                xthimg = myAnim.getMovers().get(i);
-                break;
-            }
-        }
-        if (xthimg != null) {
-            PopupDialoger pd = new PopupDialoger(this, screenRect);
-            //noinspection unused
-            JDialog jd = pd.createFull(xthimg, xpos, ypos);
-        }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    //pause animation
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        myAnim.stop();
-    }
-
-    //restart animation
-    @Override
-    public void mouseExited(MouseEvent e) {
-        myAnim.start();
     }
 
     private TransformAnim getMyAnim() {
@@ -137,24 +83,5 @@ class ShadowsFrame extends JFrame implements MouseListener, KeyListener {
             frame.getMyAnim().collectImages();
         }
         frame.getMyAnim().start();
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-//        displayInfo(e, "KEY PRESSED: ");
-        int keyCode = e.getKeyCode();
-        int modifiersEx = e.getModifiersEx();
-        //ctnrl-alt-u
-        if (keyCode == 85 && modifiersEx == 640) {
-            JOptionPane.showMessageDialog(this, "Created by Bill Brenholtz");
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
     }
 }
