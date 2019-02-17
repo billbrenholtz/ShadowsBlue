e7ffcc55307cf3c3422a38cc758430d675a1ff8b
billb <billbrenholtz@gmail.com>
2019-02-16 17:01:22 -0700

    change thread structure

--- src/ShadowsBlue/classes/hornedowl/shadowsblue/TransformAnim.java
+++ src/ShadowsBlue/classes/hornedowl/shadowsblue/TransformAnim.java
@@ -70,8 +70,8 @@ class TransformAnim extends JPanel implements PropertyChangeListener, MouseListe
 
     private final long sleepAmount = 75;
     private int biw, bih;
-    private volatile boolean running = false;
-    private volatile Thread animationThread;
+    private boolean running = false;
+    private AnimationThread animationThread;
     private ImageResizer resizer;
     private boolean resizing = false;
     private final Rectangle screenRect;
@@ -245,102 +245,19 @@ class TransformAnim extends JPanel implements PropertyChangeListener, MouseListe
     }
 
     public void start() {
-        if (!running()) {
-            animationThread = new Thread(() -> {
-
-                while (running()) {
-                    if (!movers.isEmpty()) {
-                        //is the current BufferedImage size not same as window?
-                        //(has window been resized?)
-                        Dimension d = getSize();
-                        if (biw != d.width) {
-                            //reinitialize and remember new BufferedImage width
-                            biw = d.width;
-                        }
-                        if (bih != d.height) {
-                            //reinitialize and remember new BufferedImage height
-                            bih = d.height;
-                            //we could detect mulitple resize operations
-                            //if so kill any active resize operations until the
-                            //window has stopped resizing
-                            if (resizer != null && resizer.isAlive()) {
-                                resizer.interrupt();
-                            }
-                            //start resizing images thread
-                            resizer = new ImageResizer(movers, bih);
-                            resizing = true;
-                            resizer.start();
-                        }
-                        //if resizing thread has stopped switch the current ArrayList
-                        //with the ArrayList containing the resized copies
-                        if (resizing) {
-                            if (resizer != null && !resizer.isAlive()) {
-                                resizing = false;
-                                movers = resizer.getMoversCopy();
-                            }
-                        }
-                        //move the first image in line forward
-                        movers.get(firstInLine).step();
-                        //is the first in line now off screen?
-                        if (movers.get(firstInLine).getX() > biw) {
-                            //yes, the next Thimg in the ArrayList is now the leader
-                            firstInLine++;
-                            //handle Arraylist wraparound
-                            if (firstInLine > movers.size() - 1) {
-                                firstInLine = 0;
-                            }
-                            //move the new leader forward
-                            movers.get(firstInLine).step();
-                        }
-                        //set translation for firstInLine
-                        movers.get(firstInLine).setTranslation(biw);
-                        //set x position and translation for everyone behind leader
-                        //to end of ArrayList
-                        for (int i = firstInLine; i < movers.size(); i++) {
-                            if (i != firstInLine) {
-                                movers.get(i).setX(movers.get(i - 1).getX() - movers.get(i).getThumbWidth());
-                                movers.get(i).setTranslation(biw);
-                            }
-                        }
-                        if (firstInLine != 0) {
-                            //wrapped around so now process everyone from
-                            //ArrayList(0) up to the firstInLine
-                            for (int i = 0; i < firstInLine; i++) {
-                                if (i == 0) {
-                                    //ArrayList(0) will always be behind last
-                                    //member of the ArrayList
-                                    movers.get(0).setX(movers.get(movers.size() - 1).getX() - movers.get(0).getThumbWidth());
-                                } else {
-                                    //Everyone else is behind the index in front
-                                    //of them
-                                    movers.get(i).setX(movers.get(i - 1).getX() - movers.get(i).getThumbWidth());
-                                }
-                                movers.get(i).setTranslation(biw);
-                            }
-                        }
-                        repaint();
-                    }
-                    try {
-                        Thread.sleep(sleepAmount);
-                    } catch (InterruptedException ignored) {
-                        running = false;
-                    }
-                }
-                running = false;
-            });
-            animationThread.setPriority(Thread.MIN_PRIORITY);
-            animationThread.setName("RunAnimation");
-            animationThread.start();
-            running = true;
-        }
+        animationThread = new AnimationThread();
+        animationThread.setPriority(Thread.NORM_PRIORITY);
+        animationThread.setName("RunAnimation");
+        animationThread.start();
+        running = true;
     }
 
     public synchronized void stop() {
         if (animationThread != null) {
             running = false;
             animationThread.interrupt();
+            animationThread = null;
         }
-        animationThread = null;
     }
 
     @Override
@@ -360,6 +277,91 @@ class TransformAnim extends JPanel implements PropertyChangeListener, MouseListe
         }
     }
 
+    private class AnimationThread extends Thread {
+
+        @Override
+        public void run() {
+            while (running) {
+                if (!movers.isEmpty()) {
+                    //is the current BufferedImage size not same as window?
+                    //(has window been resized?)
+                    Dimension d = getSize();
+                    if (biw != d.width) {
+                        //reinitialize and remember new BufferedImage width
+                        biw = d.width;
+                    }
+                    if (bih != d.height) {
+                        //reinitialize and remember new BufferedImage height
+                        bih = d.height;
+                        //we could detect mulitple resize operations
+                        //if so kill any active resize operations until the
+                        //window has stopped resizing
+                        if (resizer != null && resizer.isAlive()) {
+                            resizer.interrupt();
+                        }
+                        //start resizing images thread
+                        resizer = new ImageResizer(movers, bih);
+                        resizing = true;
+                        resizer.start();
+                    }
+                    //if resizing thread has stopped switch the current ArrayList
+                    //with the ArrayList containing the resized copies
+                    if (resizing) {
+                        if (resizer != null && !resizer.isAlive()) {
+                            resizing = false;
+                            movers = resizer.getMoversCopy();
+                        }
+                    }
+                    //move the first image in line forward
+                    movers.get(firstInLine).step();
+                    //is the first in line now off screen?
+                    if (movers.get(firstInLine).getX() > biw) {
+                        //yes, the next Thimg in the ArrayList is now the leader
+                        firstInLine++;
+                        //handle Arraylist wraparound
+                        if (firstInLine > movers.size() - 1) {
+                            firstInLine = 0;
+                        }
+                        //move the new leader forward
+                        movers.get(firstInLine).step();
+                    }
+                    //set translation for firstInLine
+                    movers.get(firstInLine).setTranslation(biw);
+                    //set x position and translation for everyone behind leader
+                    //to end of ArrayList
+                    for (int i = firstInLine; i < movers.size(); i++) {
+                        if (i != firstInLine) {
+                            movers.get(i).setX(movers.get(i - 1).getX() - movers.get(i).getThumbWidth());
+                            movers.get(i).setTranslation(biw);
+                        }
+                    }
+                    if (firstInLine != 0) {
+                        //wrapped around so now process everyone from
+                        //ArrayList(0) up to the firstInLine
+                        for (int i = 0; i < firstInLine; i++) {
+                            if (i == 0) {
+                                //ArrayList(0) will always be behind last
+                                //member of the ArrayList
+                                movers.get(0).setX(movers.get(movers.size() - 1).getX() - movers.get(0).getThumbWidth());
+                            } else {
+                                //Everyone else is behind the index in front
+                                //of them
+                                movers.get(i).setX(movers.get(i - 1).getX() - movers.get(i).getThumbWidth());
+                            }
+                            movers.get(i).setTranslation(biw);
+                        }
+                    }
+                    repaint();
+                }
+                try {
+                    Thread.sleep(sleepAmount);
+                } catch (InterruptedException ignored) {
+                    running = false;
+                }
+            }
+        }
+    }
+
     public List<Thimg> getMovers() {
         return movers;
     }
@@ -374,9 +376,5 @@ class TransformAnim extends JPanel implements PropertyChangeListener, MouseListe
 
     public int getBih() {
         return bih;
-    }
-
-    private boolean running() {
-        return running;
     }
 } // End TransformAnim
