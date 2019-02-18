285c28ec3a0cbda479c233a055752653fac1530f
billb <billbrenholtz@gmail.com>
2019-02-17 21:35:49 -0700

    change thread structure

--- src/ShadowsBlue/classes/hornedowl/shadowsblue/ShadowsFrame.java
+++ src/ShadowsBlue/classes/hornedowl/shadowsblue/ShadowsFrame.java
@@ -13,34 +13,42 @@ import javax.swing.JFrame;
 
 /**
  *
- * @author liberty
+ * @author Bill Brenholtz
+ * 
+ * JFrame to hold the JPanel that scrolls the images
  */
 class ShadowsFrame extends JFrame {
 
-    private final TransformAnim myAnim;
-    private final Rectangle screenRect;
-    private Fetcher fetcher;
+    private final TransformAnim myAnim;     //JPanel with scrolling images
+    private final Rectangle screenRect;     //dimension of screen
+    private final Fetcher fetcher;          //has the data
 
     private ShadowsFrame(Fetcher f) {
 
         fetcher = f;
-        
+
+        //how much real estate do we have?
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice gd = ge.getDefaultScreenDevice();
         GraphicsConfiguration gc = gd.getDefaultConfiguration();
         screenRect = gc.getBounds();
 
+        //create the JPanel and set size to full screen width and a remembered or
+        //default height
         myAnim = new TransformAnim(this, fetcher, screenRect);
         myAnim.setPreferredSize(new Dimension(screenRect.width, myAnim.getBih()));
 
     }
 
+    /**
+     * more setup better done outside constructor
+     */
     public void init() {
         setTitle("Shadows Blue");
         setLayout(new BorderLayout());
         add("Center", myAnim);
         addWindowListener(new WindowAdapter() {
-
+            //save state for next startup
             @Override
             public void windowClosing(WindowEvent e) {
                 //save starting directory, height and first in line
@@ -50,12 +58,12 @@ class ShadowsFrame extends JFrame {
                 ms.save(myAnim.getMovers());
                 System.exit(0);
             }
-
+            //start scrolling when deiconified
             @Override
             public void windowDeiconified(WindowEvent e) {
                 myAnim.start();
             }
-
+            //stop scrolling when iconified
             @Override
             public void windowIconified(WindowEvent e) {
                 myAnim.stop();
@@ -74,8 +82,10 @@ class ShadowsFrame extends JFrame {
         frame.init();
         frame.setVisible(true);
         try {
+            //try to restore images from previous run
             f.getSerializedImages();
         } catch (IOException | ClassNotFoundException ex) {
+            //if unable try to collect again if possible
             f.collectImages(f.getPicRootDir(), frame.getMyAnim().getSize());
         }
         frame.getMyAnim().init();
